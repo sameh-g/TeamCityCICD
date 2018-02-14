975f039b1ae4bdf16a0deda8e2a58ffe4030e566
Waleed <waleed.badri@itworx.com>
2018-02-14 16:35:52 +0200

    Waleed New Automation

--- SamehAutomation/Dummy.cs
+++ SamehAutomation/Dummy.cs
@@ -82,6 +82,28 @@ namespace SamehAutomation
 
 
         }
+        [Test]
+        public void WaleedAuto1()
+        {
+
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.amazon.com/");
+            driver.Manage().Window.Maximize();
+
+            IWebElement searchElement = driver.FindElement(By.Id("twotabsearchtextbox"));
+            searchElement.SendKeys("PS4");
+            searchElement.SendKeys(Keys.Enter);
+            IWebElement searchLink = driver.FindElement(By.LinkText("PlayStation 4 Slim 1TB Console"));
+            searchLink.Click();
+            IWebElement productTitle = driver.FindElement(By.Id("productTitle"));
+            string ptxt =productTitle.Text;
+            
+
+            Assert.AreEqual("PlayStation 4 Slim 1TB Console",ptxt);
+            driver.Quit();
+
+
+        }
 
     }
 }
