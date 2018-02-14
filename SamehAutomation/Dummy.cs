b3fe4cf2feff2f1ef0d53f323d8c28dbf23ae47e
ITWORX\sameh.george <sameh.george@itwrox.com>
2018-02-14 12:48:08 +0200

    commit

---
+++ SamehAutomation/Dummy.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SamehAutomation
+{
+    [TestFixture]
+    public class Dummy
+    {
+
+
+        [Test]
+        public void DummyTest()
+        {
+
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.amazon.com/");
+            driver.Manage().Window.Maximize();
+
+            IWebElement searchElement = driver.FindElement(By.Id("twotabsearchtextbox"));
+            searchElement.SendKeys("PS4");
+            searchElement.SendKeys(Keys.Enter);
+            IWebElement searchLink = driver.FindElement(By.LinkText("PlayStation 4 Slim 1TB Console"));
+            searchLink.Click();
+            IWebElement productTitle = driver.FindElement(By.Id("productTitle"));
+            string ptxt =productTitle.Text;
+            
+
+            Assert.AreEqual("PlayStation 4 Slim 1TB Console",ptxt);
+            driver.Quit();
+
+
+        }
+
+    }
+}
